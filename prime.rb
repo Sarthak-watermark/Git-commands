c4a96fcd5091aea671c83a2864a2fb9878362676
nixk-watermark <nsharma@watermarkinsights.com>
2025-07-15 15:41:22 +0530

    nixk added something

--- prime.rb
+++ prime.rb
@@ -2,7 +2,8 @@ def prime?(n)
   return false if n <= 1
   return true if n == 2
   return false if n.even?
-  (3..Math.sqrt(n)).step(2).each do |i|
+  # Nikhil did something below
+  (10..Math.sqrt(n)).step(2).each do |i|
     return false if n % i == 0
   end
   true
