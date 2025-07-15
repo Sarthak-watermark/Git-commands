406699ec15def7de7b82f15abd16d88be705e62d
Sarthak-watermark <ssinghal@watermarkinsights.com>
2025-07-15 13:02:17 +0530

    first commit

---
+++ prime.rb
@@ -0,0 +1,18 @@
+def prime?(n)
+  return false if n <= 1
+  return true if n == 2
+  return false if n.even?
+  (3..Math.sqrt(n)).step(2).each do |i|
+    return false if n % i == 0
+  end
+  true
+end
+
+# Example usage:
+print "Enter a number: "
+num = gets.to_i
+if prime?(num)
+  puts "#{num} is a prime number."
+else
+  puts "#{num} is not a prime number."
+end
